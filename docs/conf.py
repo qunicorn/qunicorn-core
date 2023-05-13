337194fe43dca9a8478df11b9395ba2d63e513fb
Lukas Januschke <st161630@stud.uni-stuttgart.de>
2023-05-13 13:04:00 +0200

    Add logo to readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -199,6 +199,11 @@ if ON_READTHEDOCS:
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_logo = "qunicorn_logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 
 # -- Further extension options -----------------------------------------------
