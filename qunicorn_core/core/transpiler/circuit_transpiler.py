c28dfd8586334b2acf55bea39afb190483da2829
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2024-04-17 15:27:08 +0200

    Improve circuit transpiler documentation
    Add transpilation error
    Add doc strings
    Add optional exclusion arguments to exclude specific transpilers or formats

--- qunicorn_core/core/transpiler/circuit_transpiler.py
+++ qunicorn_core/core/transpiler/circuit_transpiler.py
@@ -13,10 +13,33 @@
 # limitations under the License.
 
 from heapq import heappush, heappop
-from typing import ClassVar, Any, Sequence, Callable, Union
+from typing import ClassVar, Any, Sequence, Callable, Union, Optional, Type, cast
+
+
+class TranspilationError(Exception):
+    """Base error for all transpilation failures."""
+
+    def __init__(self, transpiler: "CircuitTranspiler", circuit: Any, *args: object) -> None:
+        super().__init__(transpiler, circuit, *args)
+        self.transpiler = transpiler
+        self.circuit = circuit
 
 
 class CircuitTranspiler:
+    """Base class for all circuit transpilers.
+
+    To create a new transpiler, inherit from this class and implement the :py:meth:`CircuitTranspiler.transpile_circuit` method.
+
+    Example:
+
+    .. code-block:: python
+
+        class DemoTranspiler(CircuitTranspiler, source="source-format", target="target-format", cost=1):
+            unsafe = False  # set to True if transpilation may execute unsafe code
+
+            def transpile_circuit(self, circuit: Any) -> Any:
+                ...
+    """
 
     __known_formats: set[str] = set()
     __transpilers: dict[str, list["CircuitTranspiler"]] = {}
@@ -24,6 +47,7 @@ class CircuitTranspiler:
     source: ClassVar[str] = ""
     target: ClassVar[str] = ""
     cost: ClassVar[int] = 1
+    unsafe: ClassVar[bool] = False
 
     def __init_subclass__(cls, source: str, target: str, cost: int = 1) -> None:
         cls.source = source
@@ -34,14 +58,24 @@ class CircuitTranspiler:
         CircuitTranspiler.__transpilers.setdefault(source, []).append(cls())
 
     def transpile_circuit(self, circuit: Any) -> Any:
+        """Transpile the given circuit to the target format."""
         raise NotImplementedError()
 
     @staticmethod
     def get_known_formats() -> set[str]:
+        """Get all known formats (i.e., set(target_formats) + set(source_formats))."""
         return CircuitTranspiler.__known_formats.copy()
 
     @staticmethod
     def get_transpilers(source: str) -> Sequence["CircuitTranspiler"]:
+        """Get all transpilers registered for a specific source format.
+
+        Raises:
+            KeyError: If the source format is not a known format.
+
+        Returns:
+            Sequence[CircuitTranspiler]: all circuit transpilers registered for this source format (may be empty)
+        """
         try:
             return tuple(CircuitTranspiler.__transpilers[source])
         except KeyError:
@@ -51,30 +85,57 @@ class CircuitTranspiler:
 
     @staticmethod
     def _get_transpiler_chain(
-        source: Union[str, Sequence[str]], target: str, cost: Callable[["CircuitTranspiler"], int]
+        source: Union[str, Sequence[str]],
+        target: str,
+        *,
+        cost: Callable[["CircuitTranspiler"], float],
+        exclude: Optional[set[Union[str, Type["CircuitTranspiler"]]]] = None,
+        exclude_formats: Optional[set[str]] = None,
+        exclude_unsafe: bool = False,
     ) -> Sequence["CircuitTranspiler"]:
-        frontier: list[tuple[int, str, tuple[CircuitTranspiler, ...]]] = []
+        """Get a list of transpilers from source to target minimizing overall transpilation cost."""
+        if exclude_formats and target in exclude_formats:
+            raise ValueError("Cannot transpile to an excluded target format!")
+        frontier: list[tuple[float, str, tuple[CircuitTranspiler, ...]]] = []
 
         if isinstance(source, str):
-            heappush(frontier, (0, source, tuple()))
+            if not exclude_formats or source not in exclude_formats:
+                heappush(frontier, (0, source, tuple()))
         else:
             # Start search from multiple starting points at the same time
             # e.g., when multiple source formats are available
             for s in source:
-                heappush(frontier, (0, s, tuple()))
+                if not exclude_formats or s not in exclude_formats:
+                    heappush(frontier, (0, s, tuple()))
 
-        # longest possible translator chain length
+        if not frontier:
+            # frontier is empty because all source formats are excluded formats
+            raise ValueError("Cannot transpile from an excluded source format!")
+
+        # longest possible transpiler chain length
         max_len = len(CircuitTranspiler.__known_formats)
 
         # A* using a heap with "cost" as the heuristic
         while frontier:
             current = heappop(frontier)
+
+            # check for solution
             if current[1] == target:
                 return current[2]
+
+            # expand frontier
             if len(current[2]) >= max_len:
-                continue  # a translator chain cannot be longer than the max linear chain
-            transpilers: Sequence["CircuitTranspiler"] = CircuitTranspiler.__transpilers.get(current[1], tuple())
+                continue  # a transpiler chain cannot be longer than the max linear chain
+            transpilers = cast(Sequence["CircuitTranspiler"], CircuitTranspiler.__transpilers.get(current[1], tuple()))
             for transpiler in transpilers:
+                if exclude_unsafe and transpiler.unsafe:
+                    continue  # transpiler is not safe, i.e., may execute arbitrary code
+                if exclude_formats and transpiler.target in exclude_formats:
+                    continue  # format is excluded from transpilation
+                if exclude:
+                    t_class = type(transpiler)
+                    if t_class in exclude or t_class.__name__ in exclude or t_class.__qualname__ in exclude:
+                        continue  # transpiler was specifically excluded
                 if transpiler in current[2]:
                     continue  # transpiler was already used in this chain
                 heappush(frontier, (current[0] + cost(transpiler), transpiler.target, (*current[2], transpiler)))
@@ -82,15 +143,67 @@ class CircuitTranspiler:
         raise KeyError(f"There is no transpilation path from '{source}' to '{target}'.")
 
     @staticmethod
-    def get_transpilers_limit_depth(source: Union[str, Sequence[str]], target: str) -> Sequence["CircuitTranspiler"]:
-        return CircuitTranspiler._get_transpiler_chain(source, target, cost=lambda x: 1)
+    def get_transpilers_limit_depth(
+        source: Union[str, Sequence[str]],
+        target: str,
+        *,
+        exclude: Optional[set[Union[str, Type["CircuitTranspiler"]]]] = None,
+        exclude_formats: Optional[set[str]] = None,
+        exclude_unsafe: bool = False,
+    ) -> Sequence["CircuitTranspiler"]:
+        """Get a list of transpilers from source to target format using a constant cost of 1 for each step."""
+        return CircuitTranspiler._get_transpiler_chain(
+            source,
+            target,
+            cost=lambda x: 1,
+            exclude=exclude,
+            exclude_formats=exclude_formats,
+            exclude_unsafe=exclude_unsafe,
+        )
 
     @staticmethod
-    def get_transpilers_limit_cost(source: Union[str, Sequence[str]], target: str) -> Sequence["CircuitTranspiler"]:
-        return CircuitTranspiler._get_transpiler_chain(source, target, cost=lambda x: x.cost)
+    def get_transpilers_limit_cost(
+        source: Union[str, Sequence[str]],
+        target: str,
+        *,
+        exclude: Optional[set[Union[str, Type["CircuitTranspiler"]]]] = None,
+        exclude_formats: Optional[set[str]] = None,
+        exclude_unsafe: bool = False,
+    ) -> Sequence["CircuitTranspiler"]:
+        """Get a list of transpilers from source to target format using the transpiler cost."""
+        return CircuitTranspiler._get_transpiler_chain(
+            source,
+            target,
+            cost=lambda x: x.cost,
+            exclude=exclude,
+            exclude_formats=exclude_formats,
+            exclude_unsafe=exclude_unsafe,
+        )
 
 
-def transpile_circuit(target: str, *circuit: tuple[str, Any]) -> Any:
+def transpile_circuit(
+    target: str,
+    *circuit: tuple[str, Any],
+    exclude: Optional[set[Union[str, Type[CircuitTranspiler]]]] = None,
+    exclude_formats: Optional[set[str]] = None,
+    exclude_unsafe: bool = False,
+) -> Any:
+    """Transpile a circuit available in one or more source formats to a specific target format.
+
+    Args:
+        target (str): the target format for transpilation
+        exclude (set[str | Type[CircuitTranspiler]], optional): exclude specific transpilers (by class, class name or qualified class name).
+        exclude_formats (set[str], optional): exclude specific formats from transpilation. Applies to both source and target formats!
+        exclude_unsafe (bool, optional): exclude unsafe transpilers from transpilation. Defaults to False.
+
+    Raises:
+        ValueError: If no circuit is provided or either the target format or all source formats are excluded by `exclude_formats`.
+        KeyError: If no transpilation chain could be found to transpile the circuit to the target format.
+        TranspilationError: If a transpilation step fails.
+
+    Returns:
+        Any: The transpiled circuit (this may be one of the input circuits if the format matches the target format)
+    """
     if len(circuit) == 0:
         raise ValueError("Must provide a circuit to compile!")
     elif len(circuit) == 1:
@@ -103,7 +216,13 @@ def transpile_circuit(target: str, *circuit: tuple[str, Any]) -> Any:
             # return fast if target format is already available
             return c
 
-    transpiler_chain = CircuitTranspiler.get_transpilers_limit_depth(source=source_format, target=target)
+    transpiler_chain = CircuitTranspiler.get_transpilers_limit_depth(
+        source=source_format,
+        target=target,
+        exclude=exclude,
+        exclude_formats=exclude_formats,
+        exclude_unsafe=exclude_unsafe,
+    )
 
     assert len(transpiler_chain) > 0, "There should always be at least one transpiler present."
 
@@ -113,7 +232,10 @@ def transpile_circuit(target: str, *circuit: tuple[str, Any]) -> Any:
     current_circuit = next(c for s, c in circuit if s == first_circuit_format)
 
     for transpiler in transpiler_chain:
-        current_circuit = transpiler.transpile_circuit(current_circuit)
+        try:
+            current_circuit = transpiler.transpile_circuit(current_circuit)
+        except Exception as err:
+            raise TranspilationError(transpiler, current_circuit) from err
 
         if isinstance(current_circuit, (str, bytes)):
             # circuit is in a format that can be safely stored in the database
