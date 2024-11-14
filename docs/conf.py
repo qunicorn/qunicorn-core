8e0c769c57d6e057ffafbe3b87cbfa58d2358ba4
PhilWun <philipp.wundrack@live.de>
2024-11-14 11:19:15 +0100

    Add read the docs specific code to docs conf
    See <https://about.readthedocs.com/blog/2024/07/addons-by-default/> for explanation

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,14 @@ sys.path.insert(0, os.path.abspath(".."))
 
 ON_READTHEDOCS = environ.get("READTHEDOCS") == "True"
 
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = environ.get("READTHEDOCS_CANONICAL_URL", "")
+# Tell Jinja2 templates the build is running on Read the Docs
+if environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+
 # -- Project information -----------------------------------------------------
 
 current_path = Path(".").absolute()
