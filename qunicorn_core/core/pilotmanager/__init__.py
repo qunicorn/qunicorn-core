923ff174e88dff616ed0a68c7b98337f6b18515b
Sampathkumar D Itagi <56752102+SAMPATHKUMAR12@users.noreply.github.com>
2024-12-28 13:02:25 +0100

    Update __init__.py

--- qunicorn_core/core/pilotmanager/__init__.py
+++ qunicorn_core/core/pilotmanager/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import aws_pilot, base_pilot, ibm_pilot, pilot_manager, rigetti_pilot, qmware_pilot
+from . import aws_pilot, base_pilot, ibm_pilot, pilot_manager, rigetti_pilot, qmware_pilot, ionq_pilot
